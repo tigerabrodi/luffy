5ffc1177b501a0e52a13ea1bfbff2b1c86345be0
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-27 06:18:12 +0200

    setup lint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,58 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+  },
   rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-redeclare': 'error',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['is', 'should', 'has', 'are', 'can', 'was'],
+      },
+    ],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'TSEnumDeclaration',
+        message: "Don't declare enums",
+      },
+    ],
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: true,
+        checksConditionals: true,
+        checksSpreads: true,
+      },
+    ],
+    'no-await-in-loop': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['type', 'builtin', 'external', ['parent', 'sibling'], 'index'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
