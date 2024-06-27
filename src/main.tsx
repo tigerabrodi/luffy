8e00caae911e43b54a653adf9af14cd3a3a09354
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-27 06:27:20 +0200

    setup font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
+import '@fontsource-variable/overpass'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
