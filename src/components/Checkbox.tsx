efed58dd298ec5f8ef4d65ab730aaad00ad45921
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-29 12:08:36 +0200

    feat: checkbox

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,22 @@
+import * as RadixCheckbox from '@radix-ui/react-checkbox'
+import { Check } from 'lucide-react'
+
+export const Checkbox = ({
+  checked,
+  onCheckedChange,
+}: {
+  checked: boolean
+  onCheckedChange: (checked: boolean) => void
+}) => {
+  return (
+    <RadixCheckbox.Root
+      className="size-[22px] bg-transparent border-2 border-gray-800 rounded-md [&[data-state=open]]:bg-gray-800"
+      onCheckedChange={onCheckedChange}
+      checked={checked}
+    >
+      <RadixCheckbox.Indicator className="text-blue-100">
+        <Check size={16} />
+      </RadixCheckbox.Indicator>
+    </RadixCheckbox.Root>
+  )
+}
