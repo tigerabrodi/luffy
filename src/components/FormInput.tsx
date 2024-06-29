fbf904bdbb7b1457312b510987ed89642c4cbb41
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-29 16:39:08 +0200

    style: form input styles

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -26,7 +26,7 @@ export const FormInput = () => {
         value={todoValue}
         onChange={(event) => setTodoValue(event.target.value)}
         placeholder="Wash dishes"
-        className="flex-grow p-3 border border-gray-800 rounded-md text-gray-800 placeholder:opacity-80 text-lg font-medium bg-white focus:outline-none focus:shadow-md"
+        className="flex-grow px-3 py-[10px] border border-gray-800 rounded-md text-gray-800 placeholder:opacity-80 text-lg font-medium bg-white focus:outline-none focus:shadow-md"
       />
       <button
         type="submit"
